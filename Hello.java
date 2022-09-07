c93b1cf97ac0c8df6442125b5138f12798bfd20e
Atilla <jhertaas@gmail.com>
2022-09-07 14:00:07 +0200

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,10 @@ public class Hello
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world");
+        String helloWorld[] = {"H", "e", "l" ,"l", "o"};
+        for(int i = 0; i < helloWorld.length; i++) {
+            String str = helloWorld[i];
+            System.out.println(str);
+        }
 }
 }
